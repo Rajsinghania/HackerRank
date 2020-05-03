319ad5094e16b03e6fbba8f12b280e1bd78dfa89
Rajkumar <rajsinghania8899@gmail.com>
2020-05-03 10:16:25 +0530

    Solution

---
+++ Algorithms/implementions/Solution.java
@@ -0,0 +1,70 @@
+public class Solution {
+
+    // Complete the countApplesAndOranges function below.
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int applefreq=0;
+        int orangefreq=0;
+                
+     for(int i=0;i<apples.length;i++) {
+            if(apples[i]+a>=s && apples[i]+a<=t) {
+                applefreq++;
+            }
+     }
+            for(int i=0;i<oranges.length;i++) {
+                if(oranges[i]+b>=s && oranges[i]+b<=t) {
+                    orangefreq++;
+                }
+            }
+            
+        System.out.println(applefreq);    
+        System.out.println(orangefreq);
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] st = scanner.nextLine().split(" ");
+
+        int s = Integer.parseInt(st[0]);
+
+        int t = Integer.parseInt(st[1]);
+
+        String[] ab = scanner.nextLine().split(" ");
+
+        int a = Integer.parseInt(ab[0]);
+
+        int b = Integer.parseInt(ab[1]);
+
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        int[] apples = new int[m];
+
+        String[] applesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int applesItem = Integer.parseInt(applesItems[i]);
+            apples[i] = applesItem;
+        }
+
+        int[] oranges = new int[n];
+
+        String[] orangesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int orangesItem = Integer.parseInt(orangesItems[i]);
+            oranges[i] = orangesItem;
+        }
+
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        scanner.close();
+    }
+}
